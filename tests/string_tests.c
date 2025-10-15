f9be40769d019855928231f81c279fd7b8284f7c
Eric-McKinney <ericmckinney.md@gmail.com>
2025-10-15 01:53:25 -0400

    Add tests for string_copy

--- tests/string_tests.c
+++ tests/string_tests.c
@@ -44,9 +44,57 @@ static int create_string_test(int verbose) {
     return (t1 && t2 && t3 && t4 && t5) ? SUCCESS : FAILURE;
 }
 
+static int string_copy_test_helper(const char *cstr, size_t len, int verbose) {
+    String *str = create_string(cstr, len);
+    String *copy = string_copy(str);
+
+    if (cstr == NULL) {
+        return copy == NULL;
+    }
+
+    assert(str->chars != NULL);
+    assert(copy->chars != NULL);
+
+    int len_same = str->len == copy->len;
+    int chars_same = check_mem_equal(str->chars, copy->chars, str->len);
+    int memory_location_different = str != copy && str->chars != copy->chars;
+
+    if (verbose) {
+        printf("   given string: \"%s\", length: %lu\n", cstr, len);
+        if (!len_same) {
+            printf(
+                "    " COLOR_TEXT(RED, "copy's length is different") "(copy's length: %lu, orignal length: %lu)\n",
+                copy->len,
+                len
+            );
+        }
+        if (!chars_same) {
+            printf("    " COLOR_TEXT(RED, "copy's chars are different") "\n");
+        }
+        if (!memory_location_different) {
+            printf("    " COLOR_TEXT(RED, "copy shares same memory address as original") "\n");
+        }
+    }
+
+    free_string(str);
+    free_string(copy);
+
+    return len_same && chars_same && memory_location_different;
+}
+
+static int string_copy_test(int verbose) {
+    int t1 = string_copy_test_helper(NULL, 1, verbose);
+    int t2 = string_copy_test_helper("", 0, verbose);
+    int t3 = string_copy_test_helper("I <3 C", 6, verbose);
+    int t4 = string_copy_test_helper("should be simple", 16, verbose);
+
+    return (t1 && t2 && t3 && t4) ? SUCCESS : FAILURE;
+}
+
 int main() {
     TestSuite *string_tests = create_test_suite("string tests");
     suite_add_test(string_tests, "create string", create_string_test);
+    suite_add_test(string_tests, "copy string", string_copy_test);
     run_test_suite(string_tests, 0);
     free_test_suite(string_tests);
 
