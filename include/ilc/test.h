6da290e6f2e91702c7c71f44b8a91ffcdf781b4a
Eric-McKinney <ericmckinney.md@gmail.com>
2026-05-15 13:05:36 -0400

    Add function descriptions and quick reference in comments

--- include/ilc/test.h
+++ include/ilc/test.h
@@ -5,7 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SUCCESS 0  /* while opposite of C's true/false, it fits process exit codes which run_test_suite uses */
+/* while opposite of C's true/false, these align with process exit codes which
+ * run_test_suite uses because each test runs in a child process
+ */
+#define SUCCESS 0
 #define FAILURE 1
 
 #define GREEN "\033[1;32m"
@@ -24,29 +27,97 @@ typedef struct {
 
 typedef struct _test_suite TestSuite;
 
-TestSuite *create_test_suite(const char *name);
-void free_test_suite(TestSuite *suite);
-void suite_add_test(TestSuite *suite, const char *test_name,
-                    int (*run)(const void *), const void *input);
-void run_test_suite(TestSuite *suite, int verbose);
-
+/* Is set by handle_cmdline_args (if you use it) when -v,--verbose flags used.
+ * Only used within this header file. Intended use is for tests to be able to
+ * check if they should provide more verbose output.
+ */
 int VERBOSE = 0;
 
-#define RUN_TESTS(...) \
-    Test tests[] = { __VA_ARGS__ }; \
-    size_t ntests = sizeof(tests) / sizeof(Test); \
-    __ilc_test_main__(tests, ntests); \
+/****************************/
+/* FUNCTION QUICK REFERENCE */
+/****************************/
 
-#define TEST_SUITE(suite_name) static void __ilc_test_main__(Test *tests, size_t ntests) { \
-    TestSuite *suite = create_test_suite(#suite_name); \
-    size_t i; \
-    for (i = 0; i < ntests; i++) { \
-        suite_add_test(suite, tests[i].name, tests[i].run, tests[i].input); \
-    } \
-    run_test_suite(suite, VERBOSE); \
-    free_test_suite(suite); \
-}
+/*
+ * TestSuite *create_test_suite(const char *name);
+ * void free_test_suite(TestSuite *suite);
+ * void suite_add_test(TestSuite *suite, const char *test_name,
+ *                     int (*run)(const void *), const void *input);
+ * void run_test_suite(TestSuite *suite, int verbose);
+ * void handle_cmdline_args(int argc, char **argv);
+ */
 
+/*************************/
+/* MACRO QUICK REFERENCE */
+/*************************/
+
+/*
+ * TEST_SUITE(test_suite_name)
+ * RUN_TESTS(tests...)
+ */
+
+/******************************************/
+/* FUNCTION DECLARATIONS AND DESCRIPTIONS */
+/******************************************/
+
+/*
+ * Allocates and initializes a test suite with the given name.
+ *
+ * Errors (errno values):
+ *   EFAULT: name was NULL (the pointer, not the string)
+ *
+ * Returns: a pointer to the test suite on success and NULL on error.
+ */
+TestSuite *create_test_suite(const char *name);
+
+/*
+ * Frees the resources allocated for the given test suite. Do not call this
+ * function on the same test suite more than once. Do not use a test suite
+ * after it has been freed. It is safe to call this function on a NULL pointer.
+ *
+ * Errors: none.
+ *
+ * Returns: nothing.
+ */
+void free_test_suite(TestSuite *suite);
+
+/*
+ * Adds a test to the suite with given name, run function, and pointer to its
+ * input. Tests are not required to have unique names, run functions, or inputs.
+ * The run function is expected to return SUCCESS or FAILURE (macros defined at
+ * the top of this file). Other than that, what the run function does and how it
+ * uses the input are free to choose.
+ *
+ * Errors (errno values):
+ *   EFAULT: name, run, or both were NULL
+ *
+ * Returns: nothing.
+ */
+void suite_add_test(TestSuite *suite, const char *test_name,
+                    int (*run)(const void *), const void *input);
+
+/*
+ * Runs all of the tests within the given test suite. Each test is run in a
+ * child process so that segfaults or anything that would terminate the program
+ * does not prematurely end the test suite. The value returned by each test run
+ * function should use the SUCCESS or FAILURE macros or otherwise adhere to
+ * process exit code conventions so that this function can differentiate normal
+ * execution from things like segfaults.
+ *
+ * Errors (errno values):
+ *   EFAULT: suite was NULL
+ *
+ * Returns: nothing.
+ */
+void run_test_suite(TestSuite *suite, int verbose);
+
+/*
+ * Default handling of commandline arguments for only --help, -v/--verbose flags
+ * including error messages for too many arguments and invalid arguments.
+ *
+ * Errors: just pass argc and argv untouched and nothing bad can happen :)
+ *
+ * Returns: nothing.
+ */
 void handle_cmdline_args(int argc, char **argv) {
     if (argc == 2) {
         if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--verbose") == 0) {
@@ -69,6 +140,21 @@ void handle_cmdline_args(int argc, char **argv) {
         fprintf(stderr, "%s: Too many arguments\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+}
+
+#define RUN_TESTS(...) \
+    Test tests[] = { __VA_ARGS__ }; \
+    size_t ntests = sizeof(tests) / sizeof(Test); \
+    __ilc_test_main__(tests, ntests); \
+
+#define TEST_SUITE(suite_name) static void __ilc_test_main__(Test *tests, size_t ntests) { \
+    TestSuite *suite = create_test_suite(#suite_name); \
+    size_t i; \
+    for (i = 0; i < ntests; i++) { \
+        suite_add_test(suite, tests[i].name, tests[i].run, tests[i].input); \
+    } \
+    run_test_suite(suite, VERBOSE); \
+    free_test_suite(suite); \
 }
 
 #endif
